543ba4f888ecc55712b709cdf3bb5a016cd36764
MatchaBear <matchabear@users.noreply.github.com>
2021-03-18 08:08:17 +0800

    back after long time

---
+++ belajar-java-oop/src/SetelahSekianLama.java
@@ -0,0 +1,8 @@
+public class SetelahSekianLama {
+    public static void main(String[] args) {
+        int a = 1;
+        while (int b=1;a<5;b++) {
+            System.out.println("Kerangka "+b);
+        }
+    }
+}
---
+++ comp-sci-java/src/LoopExample.java
@@ -0,0 +1,10 @@
+public class LoopExample {
+    public static void main(String[] args) {
+        int count = 1;
+        while (count <= 3) {
+            System.out.println("count is: " + count);
+            count++;
+        }
+        System.out.println("Done with the loop");
+    }
+}
